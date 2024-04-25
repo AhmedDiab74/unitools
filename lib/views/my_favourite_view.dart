b5e434361becfaa6dbe3fe5b02021b9b2de3a0ca
Your Name <you@example.com>
2024-04-26 02:17:02 +0300

    finish

--- lib/views/my_favourite_view.dart
+++ lib/views/my_favourite_view.dart
@@ -1,13 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:svg_flutter/svg.dart';
+import 'package:unitools/core/constant.dart';
+import 'package:unitools/views/add_item_view.dart';
 import 'package:unitools/widgets/my_favourite_view_body.dart';
 
-class MyFavouriteView extends StatelessWidget {
+class MyFavouriteView extends StatefulWidget {
   const MyFavouriteView({super.key});
 static String routeName = "/MyFavouriteView";
+
+  @override
+  State<MyFavouriteView> createState() => _MyFavouriteViewState();
+}
+
+class _MyFavouriteViewState extends State<MyFavouriteView> {
   @override
   Widget build(BuildContext context) {
-    return  const Scaffold(
-      body: MyFavouriteViewBody(),
+    return   Scaffold(
+      bottomNavigationBar:  NavigationBar(
+        onDestinationSelected: (value) {
+           if (value == 0) {
+            
+          }
+           else if (value == 1) {
+            
+          }
+          else if (value == 2) {
+            Navigator.pushNamed(context, MyFavouriteView.routeName)
+                .whenComplete(() => setState(() {}));
+          }
+           else if (value == 3) {
+            Navigator.pushNamed(context, AddItemView.routeName);
+          }
+           else if (value == 4) {
+          }
+           else if (value == 5) {
+          }
+        },
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        selectedIndex: 2,
+        destinations: itemNavigationBar.map((item) {
+          return NavigationDestination(
+            icon: item.icon == ""
+                ? Icon(item.photo, color: Colors.red)
+                : SvgPicture.asset(item.icon),
+            label: item.title,
+          );
+        }).toList(),
+      ),
+      body: const MyFavouriteViewBody(),
     );
   }
 }
